bbaec37dd06690cc5ba59ad7ffbfe513d9ab62f0
Joshua Gonzales <jjg6470@psu.edu>
2023-01-26 11:11:27 -0500

    Merge remote-tracking branch 'origin/main'

--- src/GiantSpider.java
+++ src/GiantSpider.java
@@ -1,7 +1,7 @@
 public class GiantSpider extends Enemy{
     //Same thing...Only a constructor
     public GiantSpider(){
-        super("Giant Spider",10,2);
+        super("Giant Spider","An arachnid that hates people.",10,2);
     }
 
 }
