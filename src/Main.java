44b42a4f7e68d00bad30fa16a6cc069679a37b53
Joshua Gonzales <jjg6470@psu.edu>
2023-01-30 15:31:38 -0500

    Merge remote-tracking branch 'origin/main'

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         Sword magicSword = new Sword("Magic Sword","A shiny sword with magic flowing around it.",20,40);
         Enemy greenOgre = new Enemy("Green Ogre","An ugly green ogre",30,20);
         Enemy redOgre = new Enemy("Red Ogre","An ugly red ogre",30,20);
-        Enemy slowZombie = new Enemy("Slow Zombie","A zombie who isn't really athletic",20,15);
-        Enemy fastZombie = new Enemy("Fast Zombie","A zombie who is athletic",20,15);
+        Zombie slowZombie = new Zombie("Slow Zombie","A zombie who isn't really athletic",20,15);
+        Zombie fastZombie = new Zombie("Fast Zombie","A zombie who is athletic",20,15);
         Enemy spotDog = new Enemy("Spot Dog","A spotty dog who isn't very friendly",20,15);
         Enemy smallPixie = new Enemy("Small Pixie","A small mythical creature who doesn't seem to like you.",15,3);
 
--- src/Zombie.java
+++ src/Zombie.java
@@ -1,5 +1,8 @@
 public class Zombie extends Enemy{
     //Same thing...Only a constructor
+    public Zombie(String name,String description,int hp,int damage){
+        super(name,description,hp,damage);
+    }
     public Zombie(){
         super("Zombie","An undead green rotting foe.",20,15);
     }
