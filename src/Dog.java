411fbbfdaedb0d9db8b314748c399c33d2fb5cad
Joshua Gonzales <jjg6470@psu.edu>
2023-01-30 15:34:50 -0500

    Merge remote-tracking branch 'origin/main'

--- src/Dog.java
+++ src/Dog.java
@@ -1,5 +1,8 @@
 public class Dog extends Enemy{
-    //Same thing...Only a constructor
+    //Same thing...Only a constructor just overloaded
+    public Dog(String name,String description,int hp,int damage) {
+        super(name, description, hp, damage);
+    }
     public Dog(){
         super("Dog","A dog who isn't very friendly",20,15);
     }
