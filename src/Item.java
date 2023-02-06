cd3dfefbbb0f8d46970d6c7bc7f854d72b5098a4
Joshua Gonzales <jjg6470@psu.edu>
2023-02-06 13:30:04 -0500

    Merge remote-tracking branch 'origin/main'

--- src/Item.java
+++ src/Item.java
@@ -3,11 +3,13 @@ public class Item {
     protected String description;
     protected int value;
 
+    //
     public Item(String name,String description, int value){
         this.name = name;
         this.description = description;
         this.value=value;
     }
+
     public String toString(){
         return String.format("%s \n=====\n %s \nValue: %d \n", this.name, this.description, this.value);
     }
